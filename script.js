7ac1e125c129eb0706da065b39d312bc057c43e3
Fatima Shahzad <107702644+fatimashahzad00@users.noreply.github.com>
2023-02-04 20:07:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+                    //   08 - 10 - 2022 Class work
+
+// alert("Hello World")
+
+//Show an alert to the viewer
+alert("Hello World From Javascript");
+
+console.log("Hello Console from Javascript");
+
+console.log(prompt("What is your name?"))
